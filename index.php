1c5f877e8d864703175f5506fbbceb75f879c979
Gato Fantasma <Luized3859@gmail.com>
2024-11-28 16:47:59 -0300

    .
    .

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
     </nav>
     <!-- Seção inicial -->
     <div id="inicio">
-        <h1 class="mascara">RoTi</h1> <!-- Texto principal com efeito de máscara -->
+        <h1 class="mascara">.</h1>
+        <h1 class="mascara">.</h1>  <!-- Texto principal com efeito de máscara -->
         <p class="subtexto">"Não existe falta de tempo, existe falta de priorização"</p> <!-- Frase sem efeito -->
         <button onclick="location.href='Telalogin.html'">Vamos começar?</button> <!-- Botão que redireciona -->
     </div>
@@ -31,120 +32,8 @@
         </svg>
     </div>
     <div class="middle of the website">
-    <div class="gradiented-content pt40 new">
-    <div class="text-block-72">Fabulous awards</div>
-    <ul role="list" class="list-3 fab w-list-unstyled">
-      <li class="item-2"><img src="images/app-store.svg" loading="lazy" alt="" class="image-13">
-        <div>
-          <div class="bold-title first">Best Self-Care App 2018</div>
-          <div class="sub">App Store</div>
-        </div>
-      </li>
-      <li class="item-2"><img src="images/google-play.svg" loading="lazy" alt="" class="image-14">
-        <div>
-          <div class="bold-title second">Best App Finalist</div>
-          <div class="sub">Google Play Awards</div>
-        </div>
-      </li>
-      <li class="item-2 second"><img src="images/google-material.svg" loading="lazy" alt="" class="image-15">
-        <div>
-          <div class="bold-title lastone">Design Award Winner</div>
-          <div class="sub">Google Material</div>
-        </div>
-      </li>
-    </ul>
-    <div class="awards-770 d-none">
-      <div class="aw-box mr65"><img src="images/app.svg" loading="lazy" alt="" class="award-icon">
-        <div>Best App<br>Self-Care 2018 <br>App Store</div>
-      </div>
-      <div class="aw-box mr50"><img src="images/play.svg" loading="lazy" alt="" class="award-icon">
-        <div>Best App Finalist <br>Google Play<br>Awards</div>
-      </div>
-      <div class="aw-box mb0"><img src="images/google.svg" loading="lazy" alt="" class="award-icon">
-        <div>Google Material<br>Design Award<br>Winner</div>
-      </div>
-    </div>
-    <div class="spacer70"></div>
-    <div class="app-container-global">
-      <div class="feature-block">
-        <div class="screen"><img src="images/Fabulous_Feature_1_Desktop.webp" loading="lazy" sizes="(max-width: 479px) 80vw, (max-width: 991px) 250px, 320px" srcset="images/Fabulous_Feature_1_Desktop.webp 500w, images/Fabulous_Feature_1_Desktop.webp 654w" alt="" class="image-122"></div>
-        <div class="text-feature m-top">
-          <div class="text-block-77 f-title"><h2>Coaching Library 24/7</h2></div>
-          <div class="text-block-78 f-subtitle">Get a boost in 2 minutes. On demand series available all day, all night.</div>
-        </div>
-      </div>
-      <div class="feature-block-inverted p-140">
-        <div class="feature-content">
-          <div class="text-block-80"><h2>Your own human coach</h2></div>
-          <div class="text-block-79">Book a session with a real live coach to inspire an inner shift.</div>
-        </div>
-        <div class="screen"><img src="images/Fabulous_Feature_2_Desktop.webp" loading="lazy" sizes="(max-width: 479px) 84vw, (max-width: 767px) 51vw, (max-width: 991px) 316px, 430px" srcset="images/Fabulous_Feature_2_Desktop-p-500.webp 500w, images/Fabulous_Feature_2_Desktop-p-800.webp 800w, images/Fabulous_Feature_2_Desktop.webp 862w" alt="" class="image-123"></div>
-      </div>
-      <div class="feature-block invertedone this">
-        <div class="text-feature m-bottom">
-          <div class="f-title"><h2>Create structure in your life</h2></div>
-          <div class="f-subtitle">Morning, afternoon, evening routines guide you through every day.</div>
-        </div>
-        <div class="screen"><img src="images/Fabulous_Feature_3.webp" loading="lazy" sizes="(max-width: 479px) 80vw, (max-width: 991px) 250px, 320px" srcset="images/Fabulous_Feature_3.webp 500w, images/Fabulous_Feature_3.png 654w" alt="" class="image-125"></div>
-      </div>
-      <div class="feature-block-inverted opppsite">
-        <div class="screen"><img src="images/Fabulous_Feature_3_Desktop.webp" loading="lazy" sizes="(max-width: 479px) 84vw, (max-width: 767px) 48vw, (max-width: 991px) 316px, 430px" srcset="images/Fabulous_Feature_3_Desktop.webp 500w, images/Fabulous_Feature_3_Desktop.webp 800w, images/Fabulous_Feature_3_Desktop.webp 984w" alt="" class="image-123 next"></div>
-        <div class="feature-content">
-          <div class="text-block-80">Coaching library on ADHD-related topics</div>
-          <div class="text-block-79">Get a boost in 2 minutes. On-demand series available all day, all night.</div>
-        </div>
-      </div>
-      <div class="feature-block">
-        <div class="screen"><img src="images/Fabulous_Feature_4_Desktop.webp" loading="lazy" sizes="(max-width: 479px) 80vw, (max-width: 991px) 250px, 320px" srcset="images/Fabulous_Feature_4_Desktop.webp 500w, images/Fabulous_Feature_4_Desktop.webp 654w" alt="" class="image-122 last"></div>
-        <div class="text-feature m-top">
-          <div class="text-block-77 f-title"><h2>Focus on deep work</h2></div>
-          <div class="text-block-78 f-subtitle">Learn how to tune out noise. Streamline your mind into a flow state.</div>
-        </div>
-      </div>
-      <div class="feature-row d-none">
-        <div class="feature-text"><img src="images/icon1.svg" loading="lazy" alt="" class="f-icon">
-          <h2 class="f-heading">Coaching library 24-7</h2>
-          <div class="text-block-10">Get a boost in 2 minutes. On demand series available all day, all night.</div>
-        </div>
-        <div class="feat-screen"><img src="images/1-min.webp" loading="lazy" alt="Coaching library 24-7" sizes="100vw" width="369" srcset="images/1-min-p-500.png 500w, images/1-min.webp 738w" class="screenshot"></div>
-      </div>
-      <div class="feature-row inverted d-none">
-        <div class="feature-text inverted"><img src="images/icon2.svg" loading="lazy" alt="" class="f-icon">
-          <h2 class="f-heading">Your own human coach</h2>
-          <div class="text-block-10">Book a session with a real live coach to inspire an inner shift.</div>
-        </div>
-        <div class="feat-screen inverted"><img src="images/masav2.webp" loading="lazy" alt="Your own human coach" sizes="100vw" width="374" srcset="images/masav2-p-500.png 500w, images/masav2.webp 748w" class="screenshot"></div>
-      </div>
-      <div class="feature-row d-none">
-        <div class="feature-text"><img src="images/icon3.svg" loading="lazy" alt="" class="f-icon">
-          <h2 class="f-heading pr40">Create structure in your life</h2>
-          <div class="text-block-10 pr40">Morning, afternoon, evening routines guide you through every day.</div>
-        </div>
-        <div class="feat-screen"><img src="images/3-min_13-min.webp" loading="lazy" alt="Create structure in your life" sizes="100vw" width="368" srcset="images/3-min_1-p-500.png 500w, images/3-min_13-min.webp 736w" class="screenshot"></div>
-      </div>
-      <div class="feature-row inverted d-none">
-        <div class="feature-text inverted"><img src="images/icon4.svg" loading="lazy" alt="" class="f-icon">
-          <h2 class="f-heading">Create healthy habits</h2>
-          <div class="text-block-10">Your habits sculpt the person you will become — choose them wisely.</div>
-        </div>
-        <div class="feat-screen inverted"><img src="images/4-min_14-min.webp" loading="lazy" alt="Create healthy habits" sizes="100vw" width="369" srcset="images/4-min_1-p-500.png 500w, images/4-min_14-min.webp 738w" class="screenshot"></div>
-      </div>
-      <div class="feature-row d-none">
-        <div class="feature-text"><img src="images/icon5.svg" loading="lazy" alt="" class="f-icon">
-          <h1 class="f-heading pr40">Join a community</h1>
-          <div class="text-block-10 pr40">Give and get support with other members just like you.</div>
-        </div>
-        <div class="feat-screen"><img src="images/5-min.webp" loading="lazy" alt="Join a community" sizes="100vw" width="369" srcset="images/5-min-p-500.png 500w, images/5-min.webp 738w" class="screenshot"></div>
-      </div>
-      <div class="feature-row inverted mb-t-0 d-none">
-        <div class="feature-text inverted"><img src="images/icon6.svg" loading="lazy" alt="" class="f-icon">
-          <h2 class="f-heading">Focus on deep work</h2>
-          <div class="text-block-10">Learn how to tune out noise. Streamline your mind into a flow state.</div>
-        </div>
-        <div class="feat-screen inverted"><img src="images/6-min.webp" loading="lazy" alt="Focus on deep work" sizes="100vw" width="369" srcset="images/6-min-p-500.png 500w, images/6-min.webp 738w" class="screenshot"></div>
-      </div>
-    </div>
-  </div>
+        <div
+    
     </div>
 </body>
 </html>
